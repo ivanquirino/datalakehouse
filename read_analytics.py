fc3320368059cfea8d5a740a7626a92f5ee0e511
Ivan Quirino <ivanquirino1928@gmail.com>
2026-05-30 00:18:25 +0200

    read analytics script

---
+++ read_analytics.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from pyspark.sql import SparkSession
+
+def run():
+    spark = SparkSession.builder.appName("ReadAnalytics").getOrCreate()
+
+    df = spark.read.parquet("datalake/ouro/metricas.parquet")
+    df.createOrReplaceTempView("metricas")
+    
+    df = spark.read.parquet("datalake/ouro/relatorio_diagnosticos.parquet")
+    df.createOrReplaceTempView("relatorio_diagnosticos")
+    
+    df = spark.sql("SELECT * FROM metricas")
+    df.show()
+
+    df = spark.sql("SELECT * FROM relatorio_diagnosticos")
+    df.show()
+
+if __name__ == "__main__":
+    run()
